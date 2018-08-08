f0d23077a0255a1257efab73ce2a75192bbe9f65
Linlin <hanlinlin@live.com>
2018-08-08 16:57:46 -0400

    first commit

---
+++ Linlin-Han_Lab06_Ex01/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Linlin_Han_Lab06_Ex01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.asyncResultLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.asyncResultLabel);
+            this.groupBox1.Controls.Add(this.calculateButton);
+            this.groupBox1.Controls.Add(this.inputTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(39, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 123);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "(1) Calculate Asynchronously";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(35, 74);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(136, 33);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(179, 32);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(131, 26);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Get Factorial of:";
+            // 
+            // asyncResultLabel
+            // 
+            this.asyncResultLabel.AutoSize = true;
+            this.asyncResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.asyncResultLabel.Location = new System.Drawing.Point(177, 77);
+            this.asyncResultLabel.Name = "asyncResultLabel";
+            this.asyncResultLabel.Size = new System.Drawing.Size(0, 20);
+            this.asyncResultLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 412);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label asyncResultLabel;
+    }
+}
+
---
+++ Linlin-Han_Lab06_Ex01/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Linlin_Han_Lab06_Ex01
+{
+    public partial class Form1 : Form
+    {
+        private long n1 = 0; // initialize with first Fibonacci number
+        private long n2 = 1; // initialize with second Fibonacci number
+        private int count = 1; // current Fibonacci number to display
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // start an async Task to calculate specified Fibonacci number
+        private async void calculateButton_Click(object sender, EventArgs e)
+        {
+            // retrieve user's input as an integer
+            int number = int.Parse(inputTextBox.Text);
+
+            asyncResultLabel.Text = "Calculating...";
+
+            // Task to perform Fibonacci calculation in separate thread
+            Task<long> fibonacciTask = Task.Run(() => Factorial(number));
+
+            // wait for Task in separate thread to complete
+            await fibonacciTask;
+
+            // display result after Task in separate thread completes
+            asyncResultLabel.Text = fibonacciTask.Result.ToString();
+        }
+
+        // recursive method Fibonacci; calculates nth Fibonacci number
+        public long Factorial(long n)
+        {
+            if (n <= 1)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
